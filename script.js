e1f81e88806c6971dd60c90b8a8e01bd3d6632e2
SRE-NADH <95708236+SRE-NADH@users.noreply.github.com>
2023-07-20 17:36:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let s ="";
+  while(num>0){
+	  let b = num%2;
+	  s+=b;
+	  num=num/2;
+  }
+	return s;
 }
 
 window.decimalToBinary = decimalToBinary;
